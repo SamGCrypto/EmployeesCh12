a2e2b908a916a51c64785f8e73ef5df9151f1aac
samgcrypto <skyrealmoblivion@gmail.com>
2023-11-19 20:09:26 -0500

    Add project files.

---
+++ EmployeesCh12/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace EmployeesCh12.Models
+{
+    public class Department
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Department")]
+        public string Name { get; set; } = string.Empty;
+
+        //Navi Properties
+        public ICollection<Employee> Employees { get; set; }= new HashSet<Employee>();
+
+        //Navi Properties
+        public ICollection<DepartmentLocation> DepartmentLoctions { get; set; } = new HashSet<DepartmentLocation>();
+    }
+}
---
+++ EmployeesCh12/Models/DepartmentLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        //Composite Primary Key/Foreign key for Department
+        public int DepartmentID { get; set; }
+        //Composition Primary/Foreign key for location
+        public int LocationID {  get; set; }
+
+        //navi properties
+        public Department Department { get; set; } = null;
+        public Location Location { get; set; } = null;
+    }
+}
